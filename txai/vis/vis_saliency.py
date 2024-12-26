433ee3f5849248c1bc52858daeb5d3c214497ce2
ashleyngomez380 <ashleyngomez380@gmail.com>
2024-12-25 23:09:25 -0600

    Update vis_saliency.py

--- txai/vis/vis_saliency.py
+++ txai/vis/vis_saliency.py
@@ -9,12 +9,14 @@ def vis_one_saliency(X, exp, ax, fig, col_num):
     T, d = Xnp.shape
 
     x_range = np.arange(T)
-
-    px, py = np.meshgrid(np.linspace(min(x_range), max(x_range), len(x_range) + 1), [min(Xnp[:,i]), max(Xnp[:,i])])
-    ax[i,col_num].plot(x_range, Xnp[:,i], color = 'black')
-    cmap = ax[i,col_num].pcolormesh(px, py, np.expand_dims(enp[:,i], 0), alpha = 0.5, cmap = 'Greens')
-    fig.colorbar(cmap, ax = ax[i][col_num])
-
+    
+    for i in range(d):
+        # Assumes heatmap:
+        px, py = np.meshgrid(np.linspace(min(x_range), max(x_range), len(x_range) + 1), [min(Xnp[:,i]), max(Xnp[:,i])])
+        ax[i,col_num].plot(x_range, Xnp[:,i], color = 'black')
+        cmap = ax[i,col_num].pcolormesh(px, py, np.expand_dims(enp[:,i], 0), alpha = 0.5, cmap = 'Greens')
+        fig.colorbar(cmap, ax = ax[i][col_num])
+        
 def vis_one_saliencyAsh(X, exp, ax, fig, col_num):
     """
     Visualizes the saliency map for a single feature (col_num).
